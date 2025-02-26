6a7f693bd00d317510a98909784050329aa97124
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-02-26 13:25:20 +0500

    Register ui finished

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
-const authPages = ['/login', '/register']
+const authPages = ['/login']
 
 const Layout = () => {
 	const location = useLocation()
--- client/src/pages/Register/RegisterPage.jsx
+++ client/src/pages/Register/RegisterPage.jsx
@@ -1,5 +1,25 @@
+import { Fragment } from 'react'
+
 const Register = () => {
-	return <div>Register</div>
+	return (
+		<Fragment>
+			<div className='container'>
+				<div className='section-wrapper'>
+					<h2 className='heading'>Register</h2>
+					<form action='#'>
+						<input type='text' placeholder='Firstname' />
+						<input type='text' placeholder='Lastname' />
+						<input type='text' placeholder='Username' />
+						<input type='text' placeholder='Password' />
+						<input type='text' placeholder='Connfirm password' />
+						<button type='submit' className='btn'>
+							Login
+						</button>
+					</form>
+				</div>
+			</div>
+		</Fragment>
+	)
 }
 
 export default Register
